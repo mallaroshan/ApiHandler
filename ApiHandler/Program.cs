108da36da19f6bbb652a2c0270b3f770084c162a
roshanmalla3566@gmail.com <roshanmalla3566@gmail.com>
2026-06-30 12:05:35 +0545

    changes

--- ApiHandler/Program.cs
+++ ApiHandler/Program.cs
@@ -10,8 +10,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 
-    builder.Services.AddDbContext<ApplicationDbContext>(options =>
-        options.UseSqlServer("Server=ROSHAN\\SQLEXPRESS;Database=APIHANDLER;Trusted_Connection=True;TrustServerCertificate=True;"));
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddOpenApi();
 builder.Services.AddScoped<IConfigurationService, ConfigurationService>();
 var app = builder.Build();
