3c8e300f48b4bcec3f9cdaa4f927634a26e7c7cd
Nishesh Dangol <nishesh.dangol@arhant.com.np>
2026-06-30 09:55:58 +0545

    changes

--- ApiHandler/Program.cs
+++ ApiHandler/Program.cs
@@ -1,5 +1,6 @@
 using ApiHandler.DataAccess;
 using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,12 +11,18 @@ builder.Services.AddControllers();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer("Server=.;Database=Test;Trusted_Connection=True;TrustServerCertificate=True;"));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference(options =>
+    {
+        options.WithTitle("Aggregator API");
+        options.WithPreferredScheme("Bearer");
+    });
 }
 
 app.UseHttpsRedirection();
